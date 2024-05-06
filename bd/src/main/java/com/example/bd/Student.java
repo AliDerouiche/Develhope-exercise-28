1482c55ee24894cf23dcf5a72c1988a4bfc85306
Bisco <alidero1605@gmail.com>
2024-05-06 10:15:21 +0200

    Initial commit

---
+++ bd/src/main/java/com/example/bd/Student.java
@@ -0,0 +1,21 @@
+package com.example.bd;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "Students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "lastName", nullable = false)
+    private String lastName;
+    @Column(name = "firstName", nullable = false)
+    private String firstName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
